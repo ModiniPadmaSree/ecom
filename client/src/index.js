9c4d41133153bc460f7590124b781d17374d8ad3
ModiniPadmaSree <modinisree@gmail.com>
2026-03-04 18:19:07 +0530

    add

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css'; // Tailwind CSS
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+axios.defaults.withCredentials = true; // ✅ global
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
